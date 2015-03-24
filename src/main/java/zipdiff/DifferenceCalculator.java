ccbc67aa787cc1615e816cf98ff40184973abcf3
Aaron Cripps <acripps@gmail.com>
2015-03-23 22:54:17 -0230

    cleanup imports

--- src/main/java/zipdiff/DifferenceCalculator.java
+++ src/main/java/zipdiff/DifferenceCalculator.java
@@ -11,7 +11,6 @@ import java.io.*;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
